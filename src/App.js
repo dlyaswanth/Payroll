eb98b5ce4f410bd29c41c3d7ed57c7a30ce07e3f
Yaswanth L <65965757+dlyaswanth@users.noreply.github.com>
2021-09-02 10:42:17 +0530

    Authentication Guard Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter,Route} from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import React,{useState} from 'react'
 import './App.css';
 import SignUpForm from './Components/screens/SignUp';
 import Loginpage from './Components/screens/Login';
@@ -15,24 +17,45 @@ import PaySchedule from './Components/screens/PaySchedule';
 import Taxinfo from './Components/screens/TaxInfo';
 import {OrganizationSetup} from './Components/screens/OrganizationSetup'
 function App() {
+  // const [state,setState]=useState(false)
+  function renderList()
+  {
+    if (localStorage.getItem('company_id'))
+    {
+      // setState(true)
+      return (
+        <div>
+          <Route exact path="/"><SignUpForm/></Route>
+          <Route path="/home"><AdminHome /></Route>
+          <Route path='/login'><Loginpage /></Route>
+          <Route path="/statutory"><Statutory /></Route>
+          <Route path="/employee"><AdminEmployee /></Route>
+          <Route path="/payrun"><AdminPayrun /></Route>
+          <Route path="/reimbursements"><AdminReimbursements /></Route>
+          <Route path="/salary-revision"><AdminSalaryRevision /></Route>
+          <Route path="/support"><Support /></Route>
+          <Route path="/reports"><AdminReports /></Route>
+          <Route path="/payschedule"><PaySchedule /></Route>
+          <Route path="/organizationsetup"><OrganizationSetup /></Route>
+          <Route path="/taxinfo"><Taxinfo /></Route>
+          <Route exact path="/salary"><SalaryComponents /></Route>
+          
+        </div>
+      )
+    }
+    else
+    return (
+      <div>
+        <Route exact path="/"><SignUpForm/></Route>
+        <Route path='/login'><Loginpage /></Route>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-        <Route exact path="/"><SignUpForm/></Route>
-        <Route path="/home"><AdminHome /></Route>
-        <Route path='/login'><Loginpage /></Route>
-        <Route path="/statutory"><Statutory /></Route>
-        <Route path="/employee"><AdminEmployee /></Route>
-        <Route path="/payrun"><AdminPayrun /></Route>
-        <Route path="/reimbursements"><AdminReimbursements /></Route>
-        <Route path="/salary-revision"><AdminSalaryRevision /></Route>
-        <Route path="/support"><Support /></Route>
-        <Route path="/reports"><AdminReports /></Route>
-        <Route path="/payschedule"><PaySchedule /></Route>
-        <Route path="/organizationsetup"><OrganizationSetup /></Route>
-        <Route path="/taxinfo"><Taxinfo /></Route>
-        <Route exact path="/salary"><SalaryComponents /></Route>
+        {renderList()}
         </BrowserRouter>
       </header>
     </div>
